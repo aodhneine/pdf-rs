292bcf95543e87448cdc25d5d9353370ebfeacba
Aodhnait Étain <aodhneine@pm.me>
2021-09-18 08:46:51 +0000

    Generate empty PDF file
    We can now create valid, empty PDF file which contains one page without
    anything else. Yay!
    
    Signed-off-by: Aodhnait Étain <aodhneine@pm.me>

--- src/lib.rs
+++ src/lib.rs
@@ -37,6 +37,7 @@ impl<'a> Writer<'a> {
 
 impl Writer<'_> {
 	/// Attempts to write the entire buffer into the underlying stream.
+	#[must_use]
 	pub fn write(&mut self, buf: &[u8]) -> std::io::Result<()> {
 		self.stream.write_all(buf)?;
 		// Only advance the offset if we wrote everything successfully.
@@ -62,9 +63,47 @@ impl Document {
 		writer.write(b"%PDF-1.7\n")?;
 		writer.write(&[PERCENT, 0x80, 0x81, 0x82, 0x83, NEWLINE])?;
 
-		let xref_pos = writer.pos();
+		let catalog_pos = writer.pos();
+		writer.write(b"1 0 obj\n")?;
+		writer.write(b"<<\n")?;
+		writer.write(b"/Type /Catalog\n")?;
+		writer.write(b"/Pages 2 0 R\n")?;
+		writer.write(b">>\n")?;
+		writer.write(b"endobj\n")?;
 
-		// TODO: Generate xref table...
+		let page_tree_pos = writer.pos();
+		writer.write(b"2 0 obj\n")?;
+		writer.write(b"<<\n")?;
+		writer.write(b"/Type /Pages\n")?;
+		writer.write(b"/Kids [3 0 R]\n")?;
+		writer.write(b"/Count 1\n")?;
+		writer.write(b">>\n")?;
+		writer.write(b"endobj\n")?;
+
+		let page1_pos = writer.pos();
+		writer.write(b"3 0 obj\n")?;
+		writer.write(b"<<\n")?;
+		writer.write(b"/Type /Page\n")?;
+		writer.write(b"/Parent 2 0 R\n")?;
+		writer.write(b"/MediaBox [0 0 612 792]\n")?;
+		writer.write(b"/Resources << >>\n")?;
+		writer.write(b">>\n")?;
+		writer.write(b"endobj\n")?;
+
+		let xref_pos = writer.pos();
+		writer.write(b"xref\n")?;
+		writer.write(b"0 4\n")?;
+		std::write!(writer.stream, "0000000000 65535 f\r\n")?;
+		std::write!(writer.stream, "{:0>10} 00000 n\r\n", catalog_pos)?;
+		std::write!(writer.stream, "{:0>10} 00000 n\r\n", page_tree_pos)?;
+		std::write!(writer.stream, "{:0>10} 00000 n\r\n", page1_pos)?;
+
+		// Write the document trailer.
+		writer.write(b"trailer\n")?;
+		writer.write(b"<<\n")?;
+		writer.write(b"/Size 4\n")?;
+		writer.write(b"/Root 1 0 R\n")?;
+		writer.write(b">>\n")?;
 
 		writer.write(b"startxref\n")?;
 		std::write!(writer.stream, "{}\n", xref_pos)?;
