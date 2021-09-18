08558d7eec92ddc0aaa7f350a77a1e6d9f266f82
Aodhnait Étain <aodhneine@pm.me>
2021-09-18 19:43:04 +0000

    Now we can draw text in the PDF
    Signed-off-by: Aodhnait Étain <aodhneine@pm.me>

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,9 @@
 // For the reference used in this project, see:
 // https://www.adobe.com/content/dam/acom/en/devnet/pdf/pdfs/pdf_reference_1-7.pdf
 
+// For the example PDF file used to figure out the format, see:
+// https://wwwimages2.adobe.com/content/dam/cc/en/legal/servicetou/Stock-Additional-Terms-en_US-20200615.pdf
+
 // Every PDF file starts with a single-line header, that for PDF 1.7 files should
 // be `%PDF-1.7`. If PDF file contains binary data, it should be followed by a
 // comment line with at least four binary characters, i.e. bytes >= 128.
@@ -60,7 +63,10 @@ impl Document {
 		const NEWLINE: u8 = 0x0a;
 		const PERCENT: u8 = 0x25;
 
+		// Write PDF header.
 		writer.write(b"%PDF-1.7\n")?;
+		// Mark the file as containing binary data, because we want to be able to
+		// embed fonts and images.
 		writer.write(&[PERCENT, 0x80, 0x81, 0x82, 0x83, NEWLINE])?;
 
 		let catalog_pos = writer.pos();
@@ -86,17 +92,46 @@ impl Document {
 		writer.write(b"/Type /Page\n")?;
 		writer.write(b"/Parent 2 0 R\n")?;
 		writer.write(b"/MediaBox [0.0 0.0 612.0 792.0]\n")?;
-		writer.write(b"/Resources << >>\n")?;
+		writer.write(b"/Contents 4 0 R\n")?;
+		writer.write(b"/Resources << /Font << /F13 5 0 R >> >>\n")?;
+		writer.write(b">>\n")?;
+		writer.write(b"endobj\n")?;
+
+		let page1_contents_pos = writer.pos();
+		writer.write(b"4 0 obj\n")?;
+		writer.write(b"<<\n")?;
+		// TODO: How can we get that length programatically?
+		writer.write(b"/Length 38\n")?;
+		writer.write(b">>\n")?;
+		writer.write(b"stream\n")?;
+		writer.write(b"BT\n")?;
+		writer.write(b"/F13 10 Tf\n")?;
+		writer.write(b"16 16 Td\n")?;
+		writer.write(b"(Hello!) Tj\n")?;
+		writer.write(b"ET\n")?;
+		writer.write(b"endstream\n")?;
+		writer.write(b"endobj\n")?;
+
+		let font_pos = writer.pos();
+		writer.write(b"5 0 obj\n")?;
+		writer.write(b"<<\n")?;
+		writer.write(b"/Type /Font\n")?;
+		writer.write(b"/Subtype /Type1\n")?;
+		writer.write(b"/Name /F13\n")?;
+		writer.write(b"/BaseFont /Helvetica\n")?;
+		writer.write(b"/Encoding /MacRomanEncoding\n")?;
 		writer.write(b">>\n")?;
 		writer.write(b"endobj\n")?;
 
 		let xref_pos = writer.pos();
 		writer.write(b"xref\n")?;
-		writer.write(b"0 4\n")?;
+		writer.write(b"0 6\n")?;
 		std::write!(writer.stream, "0000000000 65535 f\r\n")?;
 		std::write!(writer.stream, "{:0>10} 00000 n\r\n", catalog_pos)?;
 		std::write!(writer.stream, "{:0>10} 00000 n\r\n", page_tree_pos)?;
 		std::write!(writer.stream, "{:0>10} 00000 n\r\n", page1_pos)?;
+		std::write!(writer.stream, "{:0>10} 00000 n\r\n", page1_contents_pos)?;
+		std::write!(writer.stream, "{:0>10} 00000 n\r\n", font_pos)?;
 
 		// Write the document trailer.
 		writer.write(b"trailer\n")?;
